af65ccefbda0ea56336f7719b404da56f39c58bf
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-05-03 14:23:22 +0200

    Implemented training process in LinearRegression

--- linear_regression.py
+++ linear_regression.py
@@ -8,5 +8,17 @@ class LinearRegression():
   def __call__(self, X):
     return self.slope * X + self.intercept
 
+  def train(self, X, Y, epochs=10):
+    for epoch in range(epochs):
+      current_loss = model.loss_value(model(X_train), Y_train)
+      model.train_step(X_train, Y_train)
+  
+  def train_step(self, X, Y):
+    with tf.GradientTape() as tape:
+      current_loss = self.loss_value(self(X), Y)
+      gradient_slope, gradient_intercept = tape.gradient(current_loss, [self.slope, self.intercept])
+      self.slope.assign_sub(self.learning_rate * gradient_slope)
+      self.intercept.assign_sub(self.learning_rate * gradient_intercept)
+
   def loss_value(self, prediction, reality):
     return tf.reduce_mean(tf.square(prediction - reality))
