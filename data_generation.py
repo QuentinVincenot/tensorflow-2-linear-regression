5edd2507a5edafbc2cbe28e7e01bfc0ad229bf06
Quentin Vincenot <quentin.vincenot@gmail.com>
2023-05-29 18:02:11 +0200

    Fixed little spacing and comments

--- data_generation.py
+++ data_generation.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def generate_random_dataset(datapoints=250):
-	# Generate random values and noise
-	X = np.random.randn(datapoints, 1)
-	RX = np.random.randn(datapoints, 1)*np.random.randn()
-	# Generate random linear-like noisy data
-	Y = 2*X + RX
-	return X, Y
+    # Generate random values and noise
+    X = np.random.randn(datapoints, 1)
+    RX = np.random.randn(datapoints, 1)*np.random.randn()
+    # Generate random linear-like noisy data
+    Y = 2*X + RX
+    return X, Y
